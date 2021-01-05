2af8cb584226d9b9bd708053ce41bff2c2c62b6f
Elogeek <elodiechristin@gmail.com>
2021-01-05 18:54:51 +0100

    exo 106

---
+++ HTML.js
@@ -0,0 +1,17 @@
+ export function  myDiv() {
+    let div = document.createElement("div");
+    div.style.backgroundImage = "linear-gradient(to top, #30cfd0 0%, #330867 100%)";
+    div.style.width = 300 + "px";
+}
+
+ export function Imput() {
+    let input = document.createElement("input");
+    input.type = "text";
+    input.style.borderRadius = 10 + 'px';
+    input.style.borderColor = "linear-gradient(to top, #fddb92 0%, #d1fdff 100%);"
+}
+
+ export function Label() {
+    let label = document.createElement("label");
+    label.style = " ";
+}
